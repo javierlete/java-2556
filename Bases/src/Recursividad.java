44a1ec5f0dd0e09f9402b0eacfeafce2c069d9b2
Javier Lete <javier@eskura21.com>
2023-06-16 09:19:31 +0200

    BigInteger para factorial y factorialIterativo

--- Bases/src/Recursividad.java
+++ Bases/src/Recursividad.java
@@ -1,21 +1,44 @@
+import java.math.BigInteger;
 
 public class Recursividad {
 	public static void main(String[] args) {
-		System.out.println(factorial(5));
+		BigInteger diezmil = new BigInteger("10000");
+		BigInteger resultado = factorial(diezmil);
+		String textoResultado = resultado.toString();
+		int numeroDigitos = textoResultado.length();
+
+		System.out.println(numeroDigitos);
+
+		BigInteger cienmil = new BigInteger("100000");
+		resultado = factorialIterativo(cienmil);
+		textoResultado = resultado.toString();
+		numeroDigitos = textoResultado.length();
+		
+		System.out.println(numeroDigitos);
 	}
-	
+
 	// 5! = 5*4*3*2*1
 	// 5! = 5*4!
 	// 4! = 4*3!
 	// 3! = 3*2!
 	// 2! = 2*1!
 	// 1! = 1
-	
-	private static int factorial(int numero) {
-		if(numero == 1) {
-			return 1;
+
+	private static BigInteger factorial(BigInteger numero) {
+		if (numero.compareTo(BigInteger.ONE) == 0) {
+			return BigInteger.ONE;
 		}
-		
-		return numero * factorial(numero - 1);
+
+		return numero.multiply(factorial(numero.subtract(BigInteger.ONE)));
+	}
+
+	private static BigInteger factorialIterativo(BigInteger numero) {
+		BigInteger total = BigInteger.ONE;
+
+		for (BigInteger c = BigInteger.ONE; c.compareTo(numero) <= 0; c.add(BigInteger.ONE)) {
+			total = total.multiply(c);
+		}
+
+		return total;
 	}
 }
