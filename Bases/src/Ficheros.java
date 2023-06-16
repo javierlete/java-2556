50779ce7c23f7054641db2fd1135a10a3e5c5891
Javier Lete <javier@eskura21.com>
2023-06-16 10:52:31 +0200

    Try Catch

--- Bases/src/Ficheros.java
+++ Bases/src/Ficheros.java
@@ -2,30 +2,30 @@ import java.io.*;
 import java.util.Scanner;
 
 public class Ficheros {
-	public static void main(String[] args) throws IOException {
-		final String RUTA_FICHERO = "prueba.txt";
-		
-		FileWriter fw = new FileWriter(RUTA_FICHERO);
-		PrintWriter pw = new PrintWriter(fw);
-		
-		pw.println("Hola desde el fichero");
-		pw.println("Qué tal");
-		
-		System.out.println("Se ha escrito el fichero");
-		
-		pw.close();
-		
-		FileReader fr = new FileReader(RUTA_FICHERO);
-		Scanner sc = new Scanner(fr);
-		
-		String linea;
-		
-		while(sc.hasNextLine()) {
-			linea = sc.nextLine();
-			
-			System.out.println(linea);
+	public static void main(String[] args) {
+		final String RUTA_FICHERO = "/prueba.txt";
+
+		try (FileWriter fw = new FileWriter(RUTA_FICHERO); PrintWriter pw = new PrintWriter(fw)) {
+			pw.println("Hola desde el fichero");
+			pw.println("Qué tal");
+
+			System.out.println("Se ha escrito el fichero");
+		} catch (FileNotFoundException e) {
+			System.out.println("No se ha podido CREAR el fichero");
+		} catch (IOException e) {
+			System.out.println("No se ha podido escribir el fichero");
 		}
-		
-		sc.close();
+
+		try (FileReader fr = new FileReader(RUTA_FICHERO); Scanner sc = new Scanner(fr)) {
+			String linea;
+
+			while (sc.hasNextLine()) {
+				linea = sc.nextLine();
+
+				System.out.println("FICHERO: " + linea);
+			}
+		} catch (IOException e) {
+			System.out.println("No se ha podido leer el fichero");
+		}
 	}
 }
