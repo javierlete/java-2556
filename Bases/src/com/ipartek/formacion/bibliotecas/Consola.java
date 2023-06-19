9dd52a4dec57d59691e9d2b8714e6fd5afbe8fd5
Javier Lete <javier@eskura21.com>
2023-06-19 11:08:57 +0200

    Nuevos métodos para leer de consola

--- Bases/src/com/ipartek/formacion/bibliotecas/Consola.java
+++ Bases/src/com/ipartek/formacion/bibliotecas/Consola.java
@@ -1,6 +1,8 @@
 package com.ipartek.formacion.bibliotecas;
 
 import java.util.Scanner;
+import java.math.BigDecimal;
+import java.time.LocalDate;
 
 public class Consola {
 	private static final Scanner SC = new Scanner(System.in);
@@ -25,10 +27,31 @@ public class Consola {
 		return entero;
 	}
 	
-	public static boolean leerBooleano(String mensaje) {
+	public static long leerLong(String mensaje) {
 		String texto = leerLinea(mensaje);
+		long l = Long.parseLong(texto);
+		
+		return l;
+	}
+	
+	public static BigDecimal leerBigDecimal(String mensaje) {
+		String texto = leerLinea(mensaje);
+		BigDecimal bd = new BigDecimal(texto);
+		
+		return bd;
+	}
+	
+	public static boolean leerBooleano(String mensaje) {
+		String texto = leerLinea(mensaje + "(s/N)");
 		boolean booleano = Utilidades.convertirABoolean(texto);
 		
 		return booleano;
+	}
+	
+	public static LocalDate leerLocalDate(String mensaje) {
+		String texto = leerLinea(mensaje);
+		LocalDate ld = LocalDate.parse(texto);
+		
+		return ld;
 	}
 }
