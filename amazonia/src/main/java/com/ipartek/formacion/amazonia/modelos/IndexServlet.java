b57bca9d36d303e695c58ed9399331cbf629d63c
Javier Lete <javier@eskura21.com>
2023-07-26 11:07:51 +0200

    Petición de artículos a la base de datos

--- amazonia/src/main/java/com/ipartek/formacion/amazonia/modelos/IndexServlet.java
+++ amazonia/src/main/java/com/ipartek/formacion/amazonia/modelos/IndexServlet.java
@@ -1,7 +1,11 @@
 package com.ipartek.formacion.amazonia.modelos;
 
 import java.io.IOException;
-import java.math.BigDecimal;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.ArrayList;
 
 import jakarta.servlet.ServletException;
@@ -14,14 +18,31 @@ import jakarta.servlet.http.HttpServletResponse;
 public class IndexServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
+	private static final String URL = "jdbc:sqlite:C:\\Users\\java\\git\\java-2556\\amazonia\\sql\\amazonia.db";
+
+	private static final String SQL_SELECT = "SELECT * FROM articulos";
+
+	static {
+		try {
+			Class.forName("org.sqlite.JDBC");
+		} catch (ClassNotFoundException e) {
+			e.printStackTrace();
+		}
+	}
+	
 	protected void doGet(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
 		var articulos = new ArrayList<Articulo>();
 
-		for (Long id = 1L; id <= 10; id++) {
-			articulos.add(new Articulo(id, "Artículo " + id, id.intValue(), new BigDecimal(id)));
+		try (Connection con = DriverManager.getConnection(URL);
+				PreparedStatement pst = con.prepareStatement(SQL_SELECT);
+				ResultSet rs = pst.executeQuery()) {
+			while(rs.next()) {
+				articulos.add(new Articulo(rs.getLong("id"), rs.getString("descripcion"), rs.getInt("estrellas"), rs.getBigDecimal("precio")));
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
 		}
-
 		request.setAttribute("articulos", articulos);
 		request.getRequestDispatcher("/index.jsp").forward(request, response);
 	}
