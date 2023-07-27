ebf1f201cdfb7d61b61dc3ba790bb017d7072780
Javier Lete <javier@eskura21.com>
2023-07-27 11:16:12 +0200

    Agregar cantidad

--- amazonia/src/main/java/com/ipartek/formacion/amazonia/modelos/Cesta.java
+++ amazonia/src/main/java/com/ipartek/formacion/amazonia/modelos/Cesta.java
@@ -9,7 +9,11 @@ public class Cesta {
 	private List<Articulo> articulos = new ArrayList<>();
 
 	public void agregarArticulo(Articulo articulo) {
-		articulos.add(articulo);
+		if(articulos.stream().anyMatch(a -> a.getId() == articulo.getId())) {
+			agregarCantidad(articulo.getId(), articulo.getCantidad());
+		} else {
+			articulos.add(articulo);
+		}
 	}
 
 	public void eliminarArticulo(Long id) {
@@ -33,17 +37,25 @@ public class Cesta {
 				(acumulado, subtotal) -> subtotal.add(acumulado));
 	}
 
+	public void agregarCantidad(Long id, Integer cantidad) {
+		Optional<Articulo> articulo = articulos.stream().filter(a -> a.getId() == id).findFirst();
+
+		if (articulo.isPresent()) {
+			articulo.get().setCantidad(cantidad + articulo.get().getCantidad());
+		}
+	}
+
 	public void cambiarCantidad(Long id, Integer cantidad) {
 		Optional<Articulo> articulo = articulos.stream().filter(a -> a.getId() == id).findFirst();
 
-//		if (articulo.isPresent()) {
-//			articulo.get().setCantidad(cantidad);
-//		}
-		
 		Articulo a = articulo.orElse(null);
 		
 		if (a != null) {
 			a.setCantidad(cantidad);
 		}
+	}
+	
+	public Integer getCantidadArticulos() {
+		return articulos.size();
 	}
 }
