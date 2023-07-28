b7cffa022534b1e45a75d5673d3058641e945620
Javier Lete <javier@eskura21.com>
2023-07-28 10:29:07 +0200

    Servicio REST cesta API

--- amazonia/src/main/java/com/ipartek/formacion/amazonia/rest/CestaApi.java
+++ amazonia/src/main/java/com/ipartek/formacion/amazonia/rest/CestaApi.java
@@ -1,9 +1,12 @@
 package com.ipartek.formacion.amazonia.rest;
 
+import java.math.BigDecimal;
+
 import com.ipartek.formacion.amazonia.modelos.Cesta;
 
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.ws.rs.Consumes;
+import jakarta.ws.rs.GET;
 import jakarta.ws.rs.PATCH;
 import jakarta.ws.rs.Path;
 import jakarta.ws.rs.PathParam;
@@ -12,17 +15,31 @@ import jakarta.ws.rs.QueryParam;
 import jakarta.ws.rs.core.Context;
 import jakarta.ws.rs.core.MediaType;
 
-@Path("articulos")
+@Path("cesta")
 @Consumes(MediaType.APPLICATION_JSON)
 @Produces(MediaType.APPLICATION_JSON)
-public class ArticulosApi {
+public class CestaApi {
+	private Cesta cesta;
+	
+	public CestaApi(@Context HttpServletRequest request) {
+		cesta = (Cesta) request.getSession().getAttribute("cesta");
+	}
+	
+	@GET
+	@Path("cantidad")
+	public Integer getCantidad() {
+		return cesta.getCantidadArticulos();
+	}
+	
+	@GET
+	@Path("total")
+	public BigDecimal getTotal() {
+		return cesta.getTotal();
+	}
+	
 	@PATCH
 	@Path("{id}")
-	public void put(@PathParam("id") Long id, @QueryParam("cantidad") Integer cantidad, @Context HttpServletRequest request) {
-		System.out.println(id);
-		System.out.println(cantidad);
-		
-		Cesta cesta = (Cesta)request.getSession().getAttribute("cesta");
+	public void actualizarCantidadArticulo(@PathParam("id") Long id, @QueryParam("cantidad") Integer cantidad) {
 		cesta.cambiarCantidad(id, cantidad);
 	}
 }
