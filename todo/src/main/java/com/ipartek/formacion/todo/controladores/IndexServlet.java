9e83ab4dbdc9598eb8641d2ab840e65105291668
java <java@DESKTOP-T6JJQ6S.ipartekaula.com>
2023-09-04 10:42:18 +0200

    Listado rellenado desde servlet

--- todo/src/main/java/com/ipartek/formacion/todo/controladores/IndexServlet.java
+++ todo/src/main/java/com/ipartek/formacion/todo/controladores/IndexServlet.java
@@ -1,40 +1,34 @@
 package com.ipartek.formacion.todo.controladores;
 
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+import com.ipartek.formacion.todo.modelos.Tarea;
+
 import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
-import java.io.IOException;
 
-/**
- * Servlet implementation class IndexServlet
- */
+@WebServlet("/index")
 public class IndexServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
-    public IndexServlet() {
-        super();
-        // TODO Auto-generated constructor stub
-    }
-
-	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
-	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
+		List<Tarea> tareas = new ArrayList<>();
+		
+		tareas.add(new Tarea(1L, "Tarea 1", false));
+		tareas.add(new Tarea(2L, "Tarea 2", true));
+		tareas.add(new Tarea(3L, "Tarea 3", true));
+		
+		request.setAttribute("tareas", tareas);
+		
+		request.getRequestDispatcher("index.jsp").forward(request, response);
 	}
 
-	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
-	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
 		doGet(request, response);
 	}
-
 }
